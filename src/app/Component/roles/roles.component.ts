8ff4a0d0b1a99d31316b74d8b59989088e1e07a5
AB_GOWDA <abhishekgowda8090@gmail.com>
2024-10-15 22:31:05 +0530

    Added the Bootstrap and Binding is completed

--- src/app/Component/roles/roles.component.ts
+++ src/app/Component/roles/roles.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
+
+  firstName : string = "Abhishek Gowda H C";
+  angularVersion = "Angular 18";
+  AngularVersioninNumber : number = 18;
+  isValid : boolean = true;
+
+  inputType : string = "checkbox";
+
+  SelectedState :string = "";
+
+  showWelcomealert(message:string){
+    alert(message)
+
+  }
+
+  showWelcomealert2(){
+    alert("Welcome to React")
+  }
+
+  showWelcomealert3(){
+    alert("Welcome to Angular")
+  }
+
 
 }
