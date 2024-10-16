09c08724cc3a7da5152c7acefe6af55449624976
AB_GOWDA <abhishekgowda8090@gmail.com>
2024-10-16 23:10:12 +0530

    Added Master Component and render the Roles,Designation Components into this Master Component

---
+++ src/app/Component/master/master.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { RolesComponent } from '../roles/roles.component';
+import { DesignationComponent } from '../designation/designation.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-master',
+  standalone: true,
+  imports: [RolesComponent,DesignationComponent,CommonModule],
+  templateUrl: './master.component.html',
+  styleUrl: './master.component.css'
+})
+export class MasterComponent {
+
+  Component : string = "";
+
+  ChangeComponent(ChangeTab : string){
+
+    this.Component = ChangeTab;
+
+  }
+
+}
