5435c895a743c91212f718fd3a34e1b4b9e92a96
AB_GOWDA <abhishekgowda8090@gmail.com>
2024-10-17 00:46:08 +0530

    Intergrated the API for Roles ,get() API

--- src/app/Component/roles/roles.component.ts
+++ src/app/Component/roles/roles.component.ts
@@ -1,14 +1,79 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component ,inject,OnInit} from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Irole } from '../model/interface/Role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
+export class RolesComponent implements OnInit{
+
+   rolelist : Irole [] = []
+
+  http = inject(HttpClient)
+
+
+  ngOnInit(): void {
+    this.getallRoles
+  }
+
+  getallRoles(){
+    this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res:any)=>{
+      this.rolelist = res.data;
+
+    })
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   firstName : string = "Abhishek Gowda H C";
   angularVersion = "Angular 18";
